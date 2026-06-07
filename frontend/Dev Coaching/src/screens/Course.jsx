ec440366b71179e3c5890c052f351f667d25e315
Chirag Suthar <chiragvishavakarma@gmail.com>
2026-06-07 19:58:50 +0530

    Card with backend Data

--- frontend/Dev Coaching/src/screens/Course.jsx
+++ frontend/Dev Coaching/src/screens/Course.jsx
@@ -3,17 +3,17 @@ import { CiSearch } from "react-icons/ci";
 
 function Course() {
 
-    // const [Subject, setSubject] = useState([]);
+    const [Subject, setSubject] = useState([]);
 
-    // useEffect(() => {
-    //     axios.get("http://localhost:9000/api/Course")
-    //         .then((res) => {
-    //             setSubject(res.data);
-    //         })
-    //         .catch((err) => {
-    //             console.log(err);
-    //         })
-    // }, [])
+    useEffect(() => {
+        axios.get("http://localhost:9000/api/Course")
+            .then((res) => {
+                setSubject(res.data?.course || []);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, [])
 
 
     return (
