b2e68279d92b945ffe47a428873891ae97003dbc
Chirag Suthar <chiragvishavakarma@gmail.com>
2026-07-12 17:25:01 +0530

    update a vite.config files in frontend

--- frontend/Dev Coaching/vite.config.js
+++ frontend/Dev Coaching/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:9000'
+      '/api': 'https://devcoaching-83f2.onrender.com'
     },
   },
   plugins: [react(), tailwindcss(),],
